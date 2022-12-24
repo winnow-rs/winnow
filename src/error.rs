cd32f048174b82abded5ce4dbc383691a6932f4e
Ed Page <eopage@gmail.com>
2022-12-23 23:03:57 -0600

    fix(error)!: Remove VerboseError::from_char

--- src/error.rs
+++ src/error.rs
@@ -621,8 +621,6 @@ pub struct VerboseError<I> {
 pub enum VerboseErrorKind {
   /// Static string added by the `context` function
   Context(&'static str),
-  /// Indicates which character was expected by the `char` function
-  Char(char),
   /// Error kind given by various nom parsers
   Nom(ErrorKind),
 }
@@ -638,12 +636,6 @@ impl<I> ParseError<I> for VerboseError<I> {
   fn append(input: I, kind: ErrorKind, mut other: Self) -> Self {
     other.errors.push((input, VerboseErrorKind::Nom(kind)));
     other
-  }
-
-  fn from_char(input: I, c: char) -> Self {
-    VerboseError {
-      errors: vec![(input, VerboseErrorKind::Char(c))],
-    }
   }
 }
 
@@ -670,7 +662,6 @@ impl<I: fmt::Display> fmt::Display for VerboseError<I> {
     for (input, error) in &self.errors {
       match error {
         VerboseErrorKind::Nom(e) => writeln!(f, "{:?} at: {}", e, input)?,
-        VerboseErrorKind::Char(c) => writeln!(f, "expected '{}' at: {}", c, input)?,
         VerboseErrorKind::Context(s) => writeln!(f, "in section '{}', at: {}", s, input)?,
       }
     }
@@ -756,9 +747,6 @@ pub fn convert_error<I: core::ops::Deref<Target = str>>(
 
     if input.is_empty() {
       match kind {
-        VerboseErrorKind::Char(c) => {
-          write!(&mut result, "{}: expected '{}', got empty input\n\n", i, c)
-        }
         VerboseErrorKind::Context(s) => write!(&mut result, "{}: in {}, got empty input\n\n", i, s),
         VerboseErrorKind::Nom(e) => write!(&mut result, "{}: in {:?}, got empty input\n\n", i, e),
       }
@@ -788,38 +776,6 @@ pub fn convert_error<I: core::ops::Deref<Target = str>>(
       let column_number = line.offset(substring) + 1;
 
       match kind {
-        VerboseErrorKind::Char(c) => {
-          if let Some(actual) = substring.chars().next() {
-            write!(
-              &mut result,
-              "{i}: at line {line_number}:\n\
-               {line}\n\
-               {caret:>column$}\n\
-               expected '{expected}', found {actual}\n\n",
-              i = i,
-              line_number = line_number,
-              line = line,
-              caret = '^',
-              column = column_number,
-              expected = c,
-              actual = actual,
-            )
-          } else {
-            write!(
-              &mut result,
-              "{i}: at line {line_number}:\n\
-               {line}\n\
-               {caret:>column$}\n\
-               expected '{expected}', got end of input\n\n",
-              i = i,
-              line_number = line_number,
-              line = line,
-              caret = '^',
-              column = column_number,
-              expected = c,
-            )
-          }
-        }
         VerboseErrorKind::Context(s) => write!(
           &mut result,
           "{i}: at line {line_number}, in {context}:\n\
