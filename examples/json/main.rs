01bd89b5ef9bf219d891f1ca5e18dec20be486a1
Ed Page <eopage@gmail.com>
2023-01-26 11:26:52 -0600

    docs: Make the json example more interactive

--- examples/json/main.rs
+++ examples/json/main.rs
@@ -3,147 +3,95 @@
 mod parser;
 
 use winnow::error::convert_error;
-use winnow::error::ErrorKind;
+use winnow::error::Error;
 use winnow::error::VerboseError;
 use winnow::prelude::*;
 
 use parser::json;
 
-fn main() {
-  let data = "  { \"a\"\t: 42,
-  \"b\": [ \"x\", \"y\", 12 ] ,
-  \"c\": { \"hello\" : \"world\"
-  }
-  } ";
+fn main() -> Result<(), lexopt::Error> {
+  let args = Args::parse()?;
 
-  println!(
-    "will try to parse valid JSON data:\n\n**********\n{}\n**********\n",
-    data
-  );
-
-  // this will print:
-  // Ok(
-  //     Object(
-  //         {
-  //             "b": Array(
-  //                 [
-  //                     Str(
-  //                         "x",
-  //                     ),
-  //                     Str(
-  //                         "y",
-  //                     ),
-  //                     Num(
-  //                         12.0,
-  //                     ),
-  //                 ],
-  //             ),
-  //             "c": Object(
-  //                 {
-  //                     "hello": Str(
-  //                         "world",
-  //                     ),
-  //                 },
-  //             ),
-  //             "a": Num(
-  //                 42.0,
-  //             ),
-  //         },
-  //     ),
-  // )
-  println!(
-    "parsing a valid file:\n{:#?}\n",
-    // `finish` handles boilerplate from calling a parser
-    json::<(&str, ErrorKind)>(data).finish()
-  );
-
-  let data = "  { \"a\"\t: 42,
+  let data = args.input.as_deref().unwrap_or_else(|| {
+    if args.invalid {
+      "  { \"a\"\t: 42,
   \"b\": [ \"x\", \"y\", 12 ] ,
   \"c\": { 1\"hello\" : \"world\"
   }
-  } ";
+  } "
+    } else {
+      "  { \"a\"\t: 42,
+  \"b\": [ \"x\", \"y\", 12 ] ,
+  \"c\": { \"hello\" : \"world\"
+  }
+  } "
+    }
+  });
 
-  println!(
-    "will try to parse invalid JSON data:\n\n**********\n{}\n**********\n",
-    data
-  );
-
-  // here we use `(Input, ErrorKind)` as error type, which is used by default
-  // if you don't specify it. It contains the position of the error and some
-  // info on which parser encountered it.
-  // It is fast and small, but does not provide much context.
-  //
-  // This will print:
-  // basic errors - `json::<(&str, ErrorKind)>(data)`:
-  // Err(
-  //     (
-  //       "1\"hello\" : \"world\"\n  }\n  } ",
-  //       Char,
-  //     ),
-  // )
-  println!(
-    "basic errors - `json::<(&str, ErrorKind)>(data)`:\n{:#?}\n",
-    json::<(&str, ErrorKind)>(data).finish()
-  );
-
-  // nom also provides `the `VerboseError<Input>` type, which will generate a sort
-  // of backtrace of the path through the parser, accumulating info on input positions
-  // and affected parsers.
-  //
-  // This will print:
-  //
-  // parsed verbose: Err(
-  //     VerboseError {
-  //         errors: [
-  //             (
-  //                 "1\"hello\" : \"world\"\n  }\n  } ",
-  //                 Char(
-  //                     '}',
-  //                 ),
-  //             ),
-  //             (
-  //                 "{ 1\"hello\" : \"world\"\n  }\n  } ",
-  //                 Context(
-  //                     "map",
-  //                 ),
-  //             ),
-  //             (
-  //                 "{ \"a\"\t: 42,\n  \"b\": [ \"x\", \"y\", 12 ] ,\n  \"c\": { 1\"hello\" : \"world\"\n  }\n  } ",
-  //                 Context(
-  //                     "map",
-  //                 ),
-  //             ),
-  //         ],
-  //     },
-  // )
-  println!(
-    "parsed verbose: {:#?}",
-    json::<VerboseError<&str>>(data).finish()
-  );
-
-  if let Err(e) = json::<VerboseError<&str>>(data).finish() {
-    // here we use the `convert_error` function, to transform a `VerboseError<&str>`
-    // into a printable trace.
-    //
-    // This will print:
-    // verbose errors - `json::<VerboseError>(data)`:
-    // 0: at line 2:
-    //   "c": { 1"hello" : "world"
-    //          ^
-    // expected '}', found 1
-    //
-    // 1: at line 2, in map:
-    //   "c": { 1"hello" : "world"
-    //        ^
-    //
-    // 2: at line 0, in map:
-    //   { "a" : 42,
-    //   ^
-    println!(
-      "verbose errors - `json::<VerboseError>(data)`:\n{}",
-      convert_error(data, e)
-    );
+  if args.verbose {
+    match json::<VerboseError<&str>>(data).finish() {
+      Ok(json) => {
+        println!("{:#?}", json);
+      }
+      Err(err) => {
+        if args.pretty {
+          println!("{}", convert_error(data, err));
+        } else {
+          println!("{:#?}", err);
+        }
+      }
+    }
+  } else {
+    match json::<Error<&str>>(data).finish() {
+      Ok(json) => {
+        println!("{:#?}", json);
+      }
+      Err(err) => {
+        println!("{:?}", err);
+      }
+    }
   }
 
-  assert!(json::<(&str, ErrorKind)>("null").finish().is_ok());
+  Ok(())
+}
+
+#[derive(Default)]
+struct Args {
+  input: Option<String>,
+  invalid: bool,
+  verbose: bool,
+  pretty: bool,
+}
+
+impl Args {
+  fn parse() -> Result<Self, lexopt::Error> {
+    use lexopt::prelude::*;
+
+    let mut res = Args::default();
+
+    let mut args = lexopt::Parser::from_env();
+    while let Some(arg) = args.next()? {
+      match arg {
+        Long("invalid") => {
+          res.invalid = true;
+        }
+        Long("verbose") => {
+          res.verbose = true;
+          // Only case where verbose matters
+          res.invalid = true;
+        }
+        Long("pretty") => {
+          res.verbose = true;
+          // Only case where pretty matters
+          res.pretty = true;
+          res.invalid = true;
+        }
+        Value(input) => {
+          res.input = Some(input.string()?);
+        }
+        _ => return Err(arg.unexpected()),
+      }
+    }
+    Ok(res)
+  }
 }
