c364af30ee9293f52c8e6ed5f2f2534b73300eeb
Ed Page <eopage@gmail.com>
2023-02-02 12:48:05 -0600

    docs(example): Open the door for more json impls

--- examples/json/bench.rs
+++ examples/json/bench.rs
@@ -1,6 +1,8 @@
 use winnow::input::Streaming;
 
+mod json;
 mod parser;
+mod parser_dispatch;
 mod parser_streaming;
 
 fn json_bench(c: &mut criterion::Criterion) {
@@ -14,6 +16,11 @@ fn json_bench(c: &mut criterion::Criterion) {
     type Error<'i> = winnow::error::Error<parser::Input<'i>>;
 
     b.iter(|| parser::json::<Error>(data).unwrap());
+  });
+  c.bench_function("dispatch", |b| {
+    type Error<'i> = winnow::error::Error<parser::Input<'i>>;
+
+    b.iter(|| parser_dispatch::json::<Error>(data).unwrap());
   });
   c.bench_function("streaming", |b| {
     type Error<'i> = winnow::error::Error<parser_streaming::Input<'i>>;
