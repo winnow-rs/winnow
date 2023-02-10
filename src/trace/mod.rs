4e6045cf2b46daf91eb616937c3e7bc9ea94055e
Ed Page <eopage@gmail.com>
2023-02-10 15:07:17 -0600

    feat(trace): New `trace` combinator
    This will report what a parser has done for easier debugging.
    
    I really hoped to have the success path highlight what of the input was
    captured but
    - That gets messy when the `input_width` isn't wide enough
    - That gets messy when the data we are consuming is in terms of the
      original input but we are manipulating the debug output

---
+++ src/trace/mod.rs
@@ -0,0 +1,41 @@
+//! Parser execution tracing
+
+#[cfg(feature = "debug")]
+mod internals;
+
+use crate::stream::Stream;
+use crate::IResult;
+use crate::Parser;
+
+#[cfg(all(feature = "debug", not(feature = "std")))]
+compile_error!("`debug` requires `std`");
+
+/// Trace the execution of the parser
+#[cfg_attr(not(feature = "debug"), allow(unused_variables))]
+pub fn trace<I: Stream, O, E>(
+    name: impl crate::lib::std::fmt::Display,
+    mut parser: impl Parser<I, O, E>,
+) -> impl FnMut(I) -> IResult<I, O, E> {
+    #[cfg(feature = "debug")]
+    {
+        let mut call_count = 0;
+        move |i| {
+            let depth = internals::Depth::new();
+            let original = i.clone();
+            internals::start(*depth, &name, call_count, &original);
+
+            let res = parser.parse_next(i);
+
+            let consumed = res.as_ref().ok().map(|(i, _)| original.offset_to(i));
+            let severity = internals::Severity::with_iresult(&res);
+            internals::end(*depth, &name, call_count, consumed, severity);
+            call_count += 1;
+
+            res
+        }
+    }
+    #[cfg(not(feature = "debug"))]
+    {
+        move |i| parser.parse_next(i)
+    }
+}
