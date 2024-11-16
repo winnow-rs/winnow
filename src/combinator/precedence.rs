4b1499df9c1d84f7a55d9dbbadc64b463b86fa38
Igor <ig_@tutamail.com>
2024-11-16 12:33:26 +0400

    remove spaces from #[doc(alias = "...")]

--- src/combinator/precedence.rs
+++ src/combinator/precedence.rs
@@ -342,8 +342,8 @@ impl_parser_for_tuple!(P1 P2 P3 P4 P5 P6 P7 P8 P9 P10 P11 P12 P13 P14 P15 P16 P1
 /// while `Pratt` parsing only relies on binding power.
 #[doc(alias = "pratt")]
 #[doc(alias = "separated")]
-#[doc(alias = "shunting yard")]
-#[doc(alias = "precedence climbing")]
+#[doc(alias = "shunting_yard")]
+#[doc(alias = "precedence_climbing")]
 #[inline(always)]
 pub fn precedence<I, ParseOperand, Operators, Operand: 'static, E>(
     mut parse_operand: ParseOperand,
