7b6e3e09094328a53f6a58afd073cb3f81ce7a50
Igor <ig_@tutamail.com>
2024-11-19 19:43:48 +0400

    update benchmark

--- examples/pratt/bench.rs
+++ examples/pratt/bench.rs
@@ -5,7 +5,7 @@ use winnow::prelude::*;
 
 fn pratt(c: &mut criterion::Criterion) {
     let i =
-        "a = 2*-2 / ( &**foo.a->p! -+1) + 3^1 / 4 == 1 * (2 - 7 + 567 *12 /2) + 3*(1+2*( 45 /2))";
+        "a = 2*-2 * (a ? 1 + 2 * 4 - --a.bar + 2 : 2) / ( &**foo.a->p! -+1) + 3^1 / 4 == 1 * (2 - 7 + 567 *12 /2) + 3*(1+2*( 45 /2))";
     parser::pratt_parser.parse(i).expect("should parse");
     c.bench_function("pratt_parser", |b| {
         b.iter(|| black_box(parser::pratt_parser.parse(i).unwrap()));
