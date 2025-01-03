0a25ceddb6be5fb6ac1d7cda035c4cb0dcace266
Ed Page <eopage@gmail.com>
2025-01-02 21:45:02 -0600

    refactor(seq): Be consistent in fragment names

--- src/macros/seq.rs
+++ src/macros/seq.rs
@@ -84,36 +84,36 @@ macro_rules! seq {
             ))
         })
     };
-    ($($name: ident)::* ( $($elements: tt)* )) => {
+    ($($name: ident)::* ( $($fields: tt)* )) => {
         $crate::combinator::trace(stringify!($($name)::*), move |input: &mut _| {
             $crate::seq_parse_tuple_fields!(
-                ( $($elements)* );
+                ( $($fields)* );
             ).map(|t| {
                 $crate::seq_init_tuple_fields!(
-                    ( $($elements)* );
+                    ( $($fields)* );
                     ( t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7, t.8, t.9, t.10, t.11, t.12, t.13, t.14, t.15, t.16, t.17, t.18, t.19, t.20 );
                     $($name)::*;
                 )
             }).parse_next(input)
         })
     };
-    (( $($elements: tt)* )) => {
+    (( $($fields: tt)* )) => {
         $crate::combinator::trace("tuple", move |input: &mut _| {
             use $crate::Parser;
             $crate::seq_parse_tuple_fields!(
-                ( $($elements)* );
+                ( $($fields)* );
             ).map(|t| {
                 $crate::seq_init_tuple_fields!(
-                    ( $($elements)* );
+                    ( $($fields)* );
                     ( t.0, t.1, t.2, t.3, t.4, t.5, t.6, t.7, t.8, t.9, t.10, t.11, t.12, t.13, t.14, t.15, t.16, t.17, t.18, t.19, t.20 );
                     ;
                 )
             }).parse_next(input)
         })
     };
-    ($($elements: tt)*) => {
+    ($($fields: tt)*) => {
         $crate::seq!((
-            $($elements)*
+            $($fields)*
         ))
     };
 }
