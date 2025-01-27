fbea2807afdf2a15bca1bcb6ec27ac1f016db961
Ed Page <eopage@gmail.com>
2025-01-27 16:17:53 -0600

    docs: Fix deprecation timing

--- src/error.rs
+++ src/error.rs
@@ -50,7 +50,7 @@ pub type Result<O, E = ContextError> = core::result::Result<O, E>;
 pub type ModalResult<O, E = ContextError> = Result<O, ErrMode<E>>;
 
 /// Deprecated, replaced with [`ModalResult`]
-#[deprecated(since = "0.6.23", note = "Replaced with ModalResult")]
+#[deprecated(since = "0.6.25", note = "Replaced with ModalResult")]
 pub type PResult<O, E = ContextError> = ModalResult<O, E>;
 
 /// Deprecated, replaced with [`ModalResult`]
