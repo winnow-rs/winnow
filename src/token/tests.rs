b6a24f7938afb8d279a3b0341a729cdbead0cc7f
Ed Page <eopage@gmail.com>
2025-01-27 21:16:36 -0600

    feat(stream): Add `literal` support for TokenSlice
    I was hoping to get `ContainsToken` support but I'm not seeing a reasonable
    way of restructuring the traits without being blocked by the compiler
    (overlapping impls) or having other side effects (less ergonomic for
    other users).
    
    `Compare` gets us the basics though.  The `Fn(&Token<'_>) -> bool`
    variant of `ContainsToken` should still work and people can add their
    own `Compare`s to get tuples working.
    
    Also, didn't see a way to get a `impl Parser for TokenKind` to work
    for similar reasons.
    
    Fixes #637

--- src/token/tests.rs
+++ src/token/tests.rs
@@ -2834,17 +2834,15 @@ fn tokenslice_literals() {
         raw: &'i str,
     }
 
-    impl<'i, 't> ContainsToken<&Token<'t>> for &'i str {
-        #[inline(always)]
-        fn contains_token(&self, token: &Token<'t>) -> bool {
-            *self == token.raw
+    impl PartialEq<&str> for Token<'_> {
+        fn eq(&self, other: &&str) -> bool {
+            self.raw == *other
         }
     }
 
-    impl<'t> ContainsToken<&Token<'t>> for TokenKind {
-        #[inline(always)]
-        fn contains_token(&self, token: &Token<'t>) -> bool {
-            *self == token.kind
+    impl PartialEq<TokenKind> for Token<'_> {
+        fn eq(&self, other: &TokenKind) -> bool {
+            self.kind == *other
         }
     }
 
@@ -2872,7 +2870,7 @@ fn tokenslice_literals() {
             &mut self,
             input: &mut TokenSlice<'i, 't>,
         ) -> TestResult<TokenSlice<'i, 't>, &'i Token<'t>> {
-            one_of(*self).parse_next(input)
+            literal(*self).parse_next(input).map(|t| &t[0])
         }
     }
 
@@ -2908,7 +2906,7 @@ fn tokenslice_literals() {
         (
             TokenKind::If,
             TokenKind::LeftParen,
-            one_of("hello"),
+            "hello",
             TokenKind::RightParen
         )
             .parse_next(&mut input),
@@ -2923,10 +2921,12 @@ Ok(
             kind: LeftParen,
             raw: "(",
         },
-        Token {
-            kind: Value,
-            raw: "hello",
-        },
+        [
+            Token {
+                kind: Value,
+                raw: "hello",
+            },
+        ],
         Token {
             kind: RightParen,
             raw: ")",
